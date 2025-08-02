90ce309e1c147006dcc1900ffed2a91ed8554ae0
Alessandro <ap.alepot@gmail.com>
2025-08-02 18:29:03 +0200

    Add Next.js configuration and GitHub Actions workflow for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,6 +11,7 @@ const nextConfig = {
     domains: ['localhost'],
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
